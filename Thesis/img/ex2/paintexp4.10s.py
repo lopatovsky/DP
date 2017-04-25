be623aedeb3d01439dff5ed18a957b30424d6764
Lukas Lopatovsky <lopatovsky@gmail.com>
2017-04-25 15:29:42 +0200

    Write exp complexity 1st part

--- Thesis/img/ex2/paintexp4.10s.py
+++ Thesis/img/ex2/paintexp4.10s.py
@@ -3,14 +3,19 @@ import matplotlib.pyplot as plt
 
 def plot_tables():
     
-    dataf = np.loadtxt("dataexp4.100s")
+    #d1 = np.loadtxt("dataexp4.1s") 
+    d80 = np.loadtxt("dataexp4.80s")
+    d100 = np.loadtxt("dataexp4.100s")
+    d10 = np.loadtxt("dataexp4.10s")
     X = [ 2**x for x in range(1,51)]
-  
+    
+    dataf = ( 8*d80 + 10*d100 + 1*d10 ) / 19
+
     #legend
     fig = plt.figure()
     ax = fig.add_subplot(111)
  
-    ax.set_xlabel('number of hidden states')
+    ax.set_xlabel('approximate length of the time intervals')
     ax.set_ylabel('time [s]')
 
     ax.set_xscale("log", nonposx='clip')
