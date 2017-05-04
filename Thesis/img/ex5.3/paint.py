df8f6e147a168ddfde2a8b54008cfd35fa7abd91
Lukas Lopatovsky <lopatovsky@gmail.com>
2017-05-04 18:47:44 +0200

    Go back into the present

--- Thesis/img/ex5.3/paint.py
+++ Thesis/img/ex5.3/paint.py
@@ -2,10 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 
-def plot_tables():
+def plot_mixed_tables():
 
 
-    d5 = np.loadtxt("d10", delimiter=',')
+    d1 = np.loadtxt("d10", delimiter=',')
+    d2 = np.loadtxt("d20", delimiter=',')
 
     fig = plt.figure()
 
@@ -13,12 +14,32 @@ def plot_tables():
     ax.set_xlabel('iterations')
     ax.set_ylabel('performance ratio')
 
-    plt.plot( d5[0], label = "train sparse"  )
-    plt.plot( d5[1], label = "test sparse"  )
-    plt.plot( d5[2], label = "train dense"  )
-    plt.plot( d5[3], label = "test dense"  )
+    plt.plot( d1[1], label = "10 sparse"  )
+    plt.plot( d1[3], label = "10 dense"  )
+    plt.plot( d2[1], label = "20 sparse"  )
+    plt.plot( d2[3], label = "20 dense"  )
     plt.legend()
     fig.show()
     input()
 
-plot_tables()
+
+def plot_tables():
+
+
+    d1 = np.loadtxt("d20", delimiter=',')
+
+    fig = plt.figure()
+
+    ax = fig.add_subplot(111)
+    ax.set_xlabel('iterations')
+    ax.set_ylabel('performance ratio')
+
+    plt.plot( d1[0], label = "train sparse"  )
+    plt.plot( d1[1], label = "test sparse"  )
+    plt.plot( d1[2], label = "train dense"  )
+    plt.plot( d1[3], label = "test dense"  )
+    plt.legend()
+    fig.show()
+    input()
+
+plot_mixed_tables()
