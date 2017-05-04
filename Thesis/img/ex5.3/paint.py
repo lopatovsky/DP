b5efbd009032f1bbb7e31d1b3507b2f6b6c86325
Lukas Lopatovsky <lopatovsky@gmail.com>
2017-05-04 11:37:02 +0200

    Edit 2.0 till 2.3.1

---
+++ Thesis/img/ex5.3/paint.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+def plot_tables():
+
+
+    d5 = np.loadtxt("d5", delimiter=',')
+
+    fig = plt.figure()
+
+    ax = fig.add_subplot(111)
+    ax.set_xlabel('iterations')
+    ax.set_ylabel('performance ratio')
+
+    plt.plot( d5[0], label = "train sparse"  )
+    plt.plot( d5[1], label = "test sparse"  )
+    plt.plot( d5[2], label = "train dense"  )
+    plt.plot( d5[3], label = "test dense"  )
+    plt.legend()
+    fig.show()
+    input()
+
+plot_tables()
