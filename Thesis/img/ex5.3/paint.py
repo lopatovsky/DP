f491c0007a0671c0bc5728456f061f56a5e6bbea
Lukas Lopatovsky <lopatovsky@gmail.com>
2017-05-05 10:12:49 +0200

    Edit 2.0 chapter 4

--- Thesis/img/ex5.3/paint.py
+++ Thesis/img/ex5.3/paint.py
@@ -5,8 +5,8 @@ from matplotlib.patches import Rectangle
 def plot_mixed_tables():
 
 
-    d1 = np.loadtxt("d10", delimiter=',')
-    d2 = np.loadtxt("d20", delimiter=',')
+    d1 = ( np.loadtxt("d10", delimiter=',') + np.loadtxt("d10_2", delimiter=',') )/2.0
+    d2 = ( np.loadtxt("d20", delimiter=',') + np.loadtxt("d20_2", delimiter=',') )/2.0
 
     fig = plt.figure()
 
@@ -14,10 +14,17 @@ def plot_mixed_tables():
     ax.set_xlabel('iterations')
     ax.set_ylabel('performance ratio')
 
-    plt.plot( d1[1], label = "10 sparse"  )
-    plt.plot( d1[3], label = "10 dense"  )
-    plt.plot( d2[1], label = "20 sparse"  )
-    plt.plot( d2[3], label = "20 dense"  )
+    #plt.plot( d1[1], label = "10 sparse"  )
+    #plt.plot( d1[3], label = "10 dense"  )
+    #plt.plot( d2[1], label = "20 sparse"  )
+    #plt.plot( d2[3], label = "20 dense"  )
+
+    plt.plot( d1[0], label = "10 sparse"  )
+    plt.plot( d1[2], label = "10 dense"  )
+    plt.plot( d2[0], label = "20 sparse"  )
+    plt.plot( d2[2], label = "20 dense"  )
+
+
     plt.legend()
     fig.show()
     input()
